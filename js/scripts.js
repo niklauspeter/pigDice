e4f38a832298154be6befe14e6cffae050f94e71
klaus <oriokiklaus@gmail.com>
2019-03-04 14:58:08 +0300

    add math.random function to generate random numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,30 @@
-var openPage = function(event){
-  alert("this is pigdice!");
-});
 
-openPage();
+
+
+
+$(document).ready(function(){
+  $("#start").click(function(){
+    $(".gamePage").toggle();
+    $(".startPage").toggle();
+    var user1 = prompt("enter player one's name!");
+    var user2 = prompt("enter player two's name!");
+    $("#playerOne").text(user1);
+    $("#playerTwo").text(user2);
+  });
+});
+$(document).ready(function(){
+$("#roll").click(function(){
+  var randomNum = Math.random()*6;
+  var roundedNum = Math.floor(randomNum);
+  $(".dice1").text(roundedNum);
+  var result= 0;
+  if (roundedNum !==1){
+    var newScore = (roundedNum+result);
+    $("#score1").text(newScore);
+  };
+  if (roundedNum ==1){
+    alert("you have got a 1, its player 2's turn")
+  };
+
+});
+});
